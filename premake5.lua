92ab33aae8072c5f2d1215eb65945221fb231e6c
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-02-11 19:49:07 +0330

    Initialize

---
+++ premake5.lua
@@ -0,0 +1,53 @@
+workspace "AsteroidSDL"
+	configurations{"Debug", "Release"}
+	platforms{"x64"}
+
+project "AsteroidSDL"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+
+
+	vpaths {
+		["deps"] = {"dependencies/**.h", "dependencies/**.hpp", "dependencies/**.c", "dependencies/**.cpp"},
+        ["src"] = { "src/**.cpp" },
+        ["include"] = { "include/**.h", "include/**.hpp" },
+    }
+
+	files{"include/**.hpp", 
+		  "include/**.h",
+		  "src/**.c",
+		  "src/**.cpp",
+		  "dependencies/SDL3/include/**.hpp",
+		  "dependencies/SDL3/include/**.h",
+		  "dependencies/imgui/**.hpp",
+		  "dependencies/imgui/**.c",
+		  "dependencies/imgui/**.cpp",
+		  "dependencies/imgui/**.h"}
+		  
+	targetdir "x64/%{cfg.buildcfg}"
+	objdir ("bin-intermediate/%{cfg.buildcfg}")
+	includedirs{"include/",
+				"dependencies/SDL3/include/",
+				"dependencies/imgui/"}
+	libdirs("dependencies/SDL3/lib/x64/")
+	links{"SDL3"}
+
+
+	filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+	filter "toolset:gcc or toolset:clang"
+    buildoptions { "-Wall", "-Wextra", "-Werror" }
+
+	filter "toolset:msc"
+		buildoptions { "/W4 /WX" }
+
+	postbuildcommands {
+        "{COPY} dependencies/SDL3/lib/x64/SDL3.dll %{cfg.targetdir}"
+    }
