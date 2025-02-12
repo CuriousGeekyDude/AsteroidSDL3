d8804dbaf502ffa9428678439a4d9c50d989ef28
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-02-12 18:41:08 +0330

    Add window flag to engine initial structure

--- include/EngineInitData.hpp
+++ include/EngineInitData.hpp
@@ -5,6 +5,7 @@
 
 #include <cinttypes>
 #include <string>
+#include <SDL3/SDL_video.h>
 
 
 namespace Asteroid
@@ -13,6 +14,7 @@ namespace Asteroid
 	{
 		uint32_t m_width{(uint32_t)512};
 		uint32_t m_height{ (uint32_t)512 };
+		SDL_WindowFlags m_windowFlags;
 		std::string m_appName;
 		std::string m_appVersion;
 		std::string m_windowTitle;
