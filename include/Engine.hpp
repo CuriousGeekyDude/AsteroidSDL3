3936dbf15d326b6f020e43e7d37774a28ca38186
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-02-12 18:28:33 +0330

    Declare Engine class

---
+++ include/Engine.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+
+
+#include "EngineInitData.hpp"
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+namespace Asteroid
+{
+
+	class Engine
+	{
+
+	public:
+
+		explicit Engine(EngineInitData&& l_initialData);
+
+		bool Init();
+
+
+		void GameLoop();
+
+
+		~Engine();
+
+	protected:
+
+
+
+
+	private:
+
+		EngineInitData m_initialData;
+
+		SDL_Renderer* m_renderer;
+		SDL_Window* m_window;
+	};
+
+
+}
