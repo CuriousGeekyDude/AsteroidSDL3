8bed171a3f0f3b101f930655fe67ddd4eb4b4cc2
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-02-16 01:40:42 +0330

    Add a field that maps texture names to their paths at init

--- include/EngineInitData.hpp
+++ include/EngineInitData.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_init.h>
+#include <unordered_map>
 
 
 namespace Asteroid
@@ -17,6 +18,7 @@ namespace Asteroid
 		uint32_t m_height{ (uint32_t)512 };
 		SDL_WindowFlags m_windowFlags{SDL_WINDOW_RESIZABLE};
 		SDL_InitFlags m_sdlInitFlags{SDL_INIT_VIDEO};
+		std::unordered_map<std::string, std::string> m_mappedTextureNamesToTheirPaths;
 		std::string m_appName;
 		std::string m_appVersion;
 		std::string m_windowTitle;
