b4d5288c69b6dcfddcf26010f1d297ec80f1666d
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-02-15 23:12:59 +0330

    Implement gpu resource manager

---
+++ include/GpuResouceManager.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+
+
+
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+
+struct SDL_Texture;
+struct SDL_Renderer;
+
+namespace Asteroid
+{
+	class GpuResourceManager
+	{
+	public:
+
+		GpuResourceManager() = default;
+
+		SDL_Texture* CreateGpuTextureReturnResource(SDL_Renderer* l_renderer
+			,const std::string& l_texturePath
+			, const std::string& l_nameToAssociateTextureWith);
+		uint32_t CreateGpuTextureReturnHandle(SDL_Renderer* l_renderer
+			,const std::string& l_texturePath
+			, const std::string& l_nameToAssociateTextureWith);
+
+
+		SDL_Texture* RetrieveGpuTexture(const std::string& l_textureName);
+		SDL_Texture* RetrieveGpuTexture(const uint32_t l_textureHandle);
+
+		~GpuResourceManager();
+
+	private:
+
+		//Allocated textures on gpu by SDL
+		std::vector<SDL_Texture*> m_gpuTextures;
+
+		//Every allocated texture on gpu has its name associated to 
+		//its index in m_gpuTextures
+		std::unordered_map<std::string, uint32_t> m_textureNamesMappedToIndices;
+
+	};
+}
---
+++ src/GpuResourceManager.cpp
@@ -0,0 +1,96 @@
+
+
+#include "GpuResouceManager.hpp"
+#include <SDL3_image/SDL_image.h>
+#include <limits>
+
+namespace Asteroid
+{
+	
+	
+	SDL_Texture* GpuResourceManager::CreateGpuTextureReturnResource
+		(SDL_Renderer* l_renderer
+		,const std::string& l_texturePath
+		, const std::string& l_nameToAssociateTextureWith)
+	{
+		SDL_Log("Attempting to load %s\n", l_texturePath.c_str());
+
+		auto* lv_gpuTexture = IMG_LoadTexture(l_renderer, l_texturePath.c_str());
+
+		if (nullptr == lv_gpuTexture) {
+			SDL_Log("Failed to load texture %s\n", l_texturePath.c_str());
+			return nullptr;
+		}
+
+		SDL_Log("%s was loaded successfully.\n", l_texturePath.c_str());
+
+		m_gpuTextures.push_back(lv_gpuTexture);
+
+		m_textureNamesMappedToIndices.insert
+		(std::pair<std::string, uint32_t>(l_nameToAssociateTextureWith, (uint32_t)(m_gpuTextures.size() - 1)));
+
+		return lv_gpuTexture;
+
+	}
+
+	uint32_t GpuResourceManager::CreateGpuTextureReturnHandle
+		(SDL_Renderer* l_renderer
+		, const std::string& l_texturePath
+		, const std::string& l_nameToAssociateTextureWith)
+	{
+		SDL_Log("Attempting to load %s\n", l_texturePath.c_str());
+
+		auto* lv_gpuTexture = IMG_LoadTexture(l_renderer, l_texturePath.c_str());
+
+		if (nullptr == lv_gpuTexture) {
+			SDL_Log("Failed to load texture %s\n", l_texturePath.c_str());
+			return std::numeric_limits<uint32_t>::max();
+		}
+
+		SDL_Log("%s was loaded successfully.\n", l_texturePath.c_str());
+
+		m_gpuTextures.push_back(lv_gpuTexture);
+
+		m_textureNamesMappedToIndices.insert
+		(std::pair<std::string, uint32_t>(l_nameToAssociateTextureWith, (uint32_t)(m_gpuTextures.size() - 1)));
+
+		return (uint32_t)(m_gpuTextures.size()-1);
+	}
+
+	SDL_Texture* GpuResourceManager::RetrieveGpuTexture(const std::string& l_textureName)
+	{
+		SDL_Log("Attempting to retrieve gpu texture %s\n", l_textureName.c_str());
+
+
+		if (m_textureNamesMappedToIndices.end() != m_textureNamesMappedToIndices.find(l_textureName)) {
+			SDL_Log("Retrieve of gpu texture %s was successfull.\n", l_textureName.c_str());
+			return m_gpuTextures[m_textureNamesMappedToIndices[l_textureName]];
+		}
+
+		SDL_Log("Retrieve of gpu texture %s failed.\n", l_textureName.c_str());
+
+		return nullptr;
+	}
+	SDL_Texture* GpuResourceManager::RetrieveGpuTexture(const uint32_t l_textureHandle)
+	{
+
+		if (false == m_gpuTextures.empty()) {
+			if (l_textureHandle >= (uint32_t)m_gpuTextures.size()) {
+				SDL_Log("Texture handle is bigger than the size of the gpu textures vector.\n");
+				return nullptr;
+			}
+			return m_gpuTextures[l_textureHandle];
+		}
+
+		return nullptr;
+	}
+	
+
+
+	GpuResourceManager::~GpuResourceManager()
+	{
+		for (auto l_gpuTexture : m_gpuTextures) {
+			SDL_DestroyTexture(l_gpuTexture);
+		}
+	}
+}
