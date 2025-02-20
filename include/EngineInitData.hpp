7dccc991a619a4856ca8941899d8edbae53ac6fe
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-02-20 09:01:16 +0330

    Eliminate SDL flags
    The user does not need to know about these nor meddle in what flags should be set to initialize the window.

--- include/EngineInitData.hpp
+++ include/EngineInitData.hpp
@@ -16,11 +16,9 @@ namespace Asteroid
 	{
 		uint32_t m_width{(uint32_t)512};
 		uint32_t m_height{ (uint32_t)512 };
-		SDL_WindowFlags m_windowFlags{SDL_WINDOW_RESIZABLE};
-		SDL_InitFlags m_sdlInitFlags{SDL_INIT_VIDEO};
-		std::unordered_map<std::string, std::string> m_mappedTextureNamesToTheirPaths;
 		std::string m_appName;
 		std::string m_appVersion;
 		std::string m_windowTitle;
+		std::unordered_map<std::string, std::string> m_mappedTextureNamesToTheirPaths;
 	};
 }
