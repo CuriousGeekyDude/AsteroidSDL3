1cc3b862891e55f9c8c79b9ead6024ad1edaad96
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-04-11 01:13:18 +0430

    Integrate Imgui

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -8,6 +8,9 @@
 
 #define LOGGING
 #include "Systems/LogSystem.hpp"
+#include <imgui.h>
+#include <backends/imgui_impl_sdl3.h>
+#include <backends/imgui_impl_sdlrenderer3.h>
 #include <SDL3/SDL.h>
 #include <memory>
 
@@ -98,6 +101,9 @@ namespace Asteroid
 
 		
 
+		
+
+
 		return true;
 	}
 
@@ -105,7 +111,24 @@ namespace Asteroid
 	bool Engine::GameLoop()
 	{
 
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsLight();
+
+		// Setup Platform/Renderer backends
+		ImGui_ImplSDL3_InitForSDLRenderer(m_window, m_renderer.GetSDLRenderer());
+		ImGui_ImplSDLRenderer3_Init(m_renderer.GetSDLRenderer());
+
+
 		bool lv_quit = false;
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.f, 0.f, 0.f, 1.00f);
 		while (false == lv_quit) {
 
 			m_trackLastFrameElapsedTime.m_currentTime = SDL_GetTicks();
@@ -113,6 +136,9 @@ namespace Asteroid
 			SDL_Event lv_event;
 
 			while (true == SDL_PollEvent(&lv_event)) {
+
+				ImGui_ImplSDL3_ProcessEvent(&lv_event);
+
 				if (SDL_EVENT_QUIT == lv_event.type) {
 					lv_quit = true;
 				}
@@ -124,8 +150,57 @@ namespace Asteroid
 			for (auto& l_entity : m_entities) {
 				assert(l_entity.Update((float)m_trackLastFrameElapsedTime.m_lastFrameElapsedTime));
 			}
-			assert(true == m_renderer.PresentToWindow());
 			
+
+
+			ImGui_ImplSDLRenderer3_NewFrame();
+			ImGui_ImplSDL3_NewFrame();
+			ImGui::NewFrame();
+
+			// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+			if (show_demo_window)
+				ImGui::ShowDemoWindow(&show_demo_window);
+
+			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+				ImGui::End();
+			}
+
+			// 3. Show another simple window.
+			if (show_another_window)
+			{
+				ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+				ImGui::Text("Hello from another window!");
+				if (ImGui::Button("Close Me"))
+					show_another_window = false;
+				ImGui::End();
+			}
+
+			// Rendering
+			ImGui::Render();
+			//SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+			
+			ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), m_renderer.GetSDLRenderer());
+
+			assert(true == m_renderer.PresentToWindow());
 
 			m_trackLastFrameElapsedTime.m_lastFrameElapsedTime = SDL_GetTicks() - m_trackLastFrameElapsedTime.m_currentTime;
 		}
