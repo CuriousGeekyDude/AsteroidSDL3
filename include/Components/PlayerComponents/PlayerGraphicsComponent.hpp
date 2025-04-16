15771266077dd51b1f3aac6e741d3a32a91c932e
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-04-16 08:21:16 +0430

    Pass the rotation info to renderer

--- include/Components/PlayerComponents/PlayerGraphicsComponent.hpp
+++ include/Components/PlayerComponents/PlayerGraphicsComponent.hpp
@@ -19,6 +19,8 @@ namespace Asteroid
 		class Renderer;
 	}
 
+	class PlayerMovementComponent;
+
 	class PlayerGraphicsComponent : public GraphicsComponent
 	{
 	
@@ -26,11 +28,13 @@ namespace Asteroid
 
 		PlayerGraphicsComponent( uint32_t l_textureHandle
 								,RenderSystem::Renderer* l_renderer
-		                        ,Entity* l_entity);
+		                        ,Entity* l_entity
+								,const PlayerMovementComponent& l_playerMovementComponent);
 
 		bool Update(float l_lastFrameElapsedTime) override;
 
 
-
+	private:
+		const PlayerMovementComponent* m_playerMovementComponent;
 	};
 }
--- src/Components/PlayerComponents/PlayerGraphicsComponent.cpp
+++ src/Components/PlayerComponents/PlayerGraphicsComponent.cpp
@@ -17,8 +17,10 @@ namespace Asteroid
 
 	PlayerGraphicsComponent::PlayerGraphicsComponent(uint32_t l_textureHandle
 	, RenderSystem::Renderer* l_renderer
-	,Entity* l_ownerEntity)
+	,Entity* l_ownerEntity
+	,const PlayerMovementComponent& l_playerMovementComponent)
 		:GraphicsComponent(l_textureHandle, l_renderer, l_ownerEntity)
+		,m_playerMovementComponent(&l_playerMovementComponent)
 	{
 
 	}
@@ -27,12 +29,16 @@ namespace Asteroid
 	bool PlayerGraphicsComponent::Update(float l_deltaTime)
 	{
 		const glm::vec2& lv_currentPos = m_ownerEntity->GetCurrentPos();
+		const float lv_theta = m_playerMovementComponent->GetCurrentAngleOfRotation();
 
 		RenderSystem::RenderingData lv_renderData{};
-		lv_renderData.m_entityPos = lv_currentPos;
-		lv_renderData.m_entityTextureHandle = m_textureHandle;
 		lv_renderData.m_widthToRender = 90;
 		lv_renderData.m_heightToRender = 90;
+		lv_renderData.m_centerOfRotation = glm::vec2{ (float)lv_renderData.m_widthToRender / 2.f, (float)lv_renderData.m_heightToRender / 2.f };
+		lv_renderData.m_entityPos = lv_currentPos - lv_renderData.m_centerOfRotation;
+		lv_renderData.m_entityTextureHandle = m_textureHandle;
+		lv_renderData.m_angleOfRotation = lv_theta;
+		lv_renderData.m_centerOfRotation = glm::vec2{ (float)lv_renderData.m_widthToRender/2.f, (float)lv_renderData.m_heightToRender/2.f};
 
 		if (false == m_renderer->RenderEntity(lv_renderData)) {
 			return false;
