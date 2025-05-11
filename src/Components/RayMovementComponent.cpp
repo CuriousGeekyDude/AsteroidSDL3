97c5d16f50c7f44a92de5b49b9341c38f0641d16
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2025-05-11 23:40:34 +0430

    Make sure there is no movem. if it is paused

--- src/Components/RayMovementComponent.cpp
+++ src/Components/RayMovementComponent.cpp
@@ -35,19 +35,21 @@ namespace Asteroid
 
 	bool RayMovementComponent::Update(UpdateComponents& l_updateContext)
 	{
-		auto& lv_ownerEntity = l_updateContext.m_engine->GetEntityFromHandle(m_ownerEntityHandle);
+
+		if (false == m_pauseMovement) {
+			auto& lv_ownerEntity = l_updateContext.m_engine->GetEntityFromHandle(m_ownerEntityHandle);
 
 
 
-		m_initialT += m_speed.x * l_updateContext.m_deltaTime;
+			m_initialT += m_speed.x * l_updateContext.m_deltaTime;
 
-		const glm::vec2 lv_newPos = m_initialPos + (m_initialT)*m_rayDirection;
-		m_transform[2][0] = lv_newPos.x;
-		m_transform[2][1] = lv_newPos.y;
+			const glm::vec2 lv_newPos = m_initialPos + (m_initialT)*m_rayDirection;
+			m_transform[2][0] = lv_newPos.x;
+			m_transform[2][1] = lv_newPos.y;
 
-		lv_ownerEntity.SetCurrentPos(lv_newPos);
+			lv_ownerEntity.SetCurrentPos(lv_newPos);
 
-
+		}
 
 		return true;
 	}
